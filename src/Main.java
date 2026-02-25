a5ab92f8572fba0420c9629ace4abf7373581428
rainboestrykr <abhirajbhowmick27@gmail.com>
2026-02-25 18:41:05 +0530

    UC12

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Deque;
 import java.util.ArrayDeque;
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class Main {
 
@@ -23,6 +26,7 @@ public class Main {
         uc9_RecursiveMethod();
         uc10_CaseInsensitiveSpaceIgnored();
         uc11_ObjectOrientedService();
+        uc12_StrategyPattern();
     }
 
     // UC2
@@ -272,10 +276,29 @@ public class Main {
     System.out.println("Is Palindrome? : " + result);
     System.out.println();
     }
+    // UC12
+    public static void uc12_StrategyPattern() {
+
+    String input = "madam";
+
+    // Choose strategy dynamically
+    PalindromeStrategy strategy = new StackStrategy();
+    // Try changing to: new DequeStrategy();
+
+    PalindromeContext context = new PalindromeContext(strategy);
+
+    boolean result = context.executeStrategy(input);
+
+    System.out.println("UC12: Strategy Pattern for Palindrome Algorithms");
+    System.out.println("Using Strategy : " + strategy.getClass().getSimpleName());
+    System.out.println("Input : " + input);
+    System.out.println("Is Palindrome? : " + result);
+    System.out.println();
+    }
 }
 
 // Separate Service Class (Encapsulation)
-    class PalindromeService {
+class PalindromeService {
 
     // Public method exposed to outside
     public boolean checkPalindrome(String input) {
@@ -301,4 +324,58 @@ public class Main {
 
         return true;
     }
+}
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray())
+            stack.push(c);
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop())
+                return false;
+        }
+
+        return true;
     }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray())
+            deque.add(c);
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast()))
+                return false;
+        }
+
+        return true;
+    }
+}
+
+class PalindromeContext {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeContext(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean executeStrategy(String input) {
+        return strategy.check(input);
+    }
+}
